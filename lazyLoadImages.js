47e5f5a2212ef5ce1929525da3c5c5f6efe29073
Nick George <Me@Izzmo.com>
2015-11-03 20:07:10 -0600

    Initial commit for lazyLoadImages

---
+++ lazyLoadImages.js
@@ -0,0 +1,51 @@
+define([], function () {
+  'use strict';
+
+  function isAboveTheFold(node) {
+    if ('object' !== typeof node) return;
+
+    var w = window.innerWidth,
+        h = window.innerHeight,
+        x = node.x,
+        y = node.y,
+        scrollX = window.scrollX,
+        scrollY = window.scrollY;
+
+    if (undefined === w || undefined === h) return;
+    if (x === undefined || y === undefined) {
+      x = node.offsetLeft;
+      y = node.offsetTop;
+    }
+    if (scrollX === undefined || scrollY === undefined) {
+      scrollX = document.documentElement.scrollLeft;
+      scrollY = document.documentElement.scrollTop;
+    }
+
+    if (node.style.display === 'none') return false;
+
+    return (x <= (w + scrollX) && y <= (h + scrollY));
+  }
+  
+  function setScrollEventHandler() {
+    if (window.hasLazyLoad) return;
+    window.addEventListener('scroll', lazyLoadImages);
+    window.addEventListener('resize', lazyLoadImages);
+    window.hasLazyLoad = true;
+  }
+
+  function lazyLoadImages() {
+    var imgs = document.querySelectorAll('img[data-src]');
+    Array.prototype.forEach.call(imgs, function lazyLoadLoop(img) {
+      var attr = img.getAttribute('data-src');
+      if (null !== attr && isAboveTheFold(img)) {
+        img.setAttribute('src', attr);
+        img.removeAttribute('data-src');
+      }
+    });
+  }
+
+  return function () {  
+    lazyLoadImages();
+    setScrollEventHandler();
+  };
+});
